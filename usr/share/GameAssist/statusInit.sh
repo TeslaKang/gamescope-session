5b003a3b50109e293ea74b4b5c171a821f72ff01
tesla.kang <kyh96403@hanmail.net>
2023-12-19 07:23:09 +0900

    fix slow problem before hibernate

--- usr/share/GameAssist/statusInit.sh
+++ usr/share/GameAssist/statusInit.sh
@@ -5,14 +5,20 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 REL_DIR=$(dirname $0)
+if [ $# -ge 1 ]; then
+    $REL_DIR/GameAssist $1
+else
+    $REL_DIR/GameAssist
+fi
+
 $REL_DIR/GameAssist off
 
 CONFIG_DIR=/home/deck/.config/GameAssist
 FILE=$CONFIG_DIR/tdp.txt
 if [ -f $FILE ]; then
     TDP=$(cat $FILE)
-    c=$(echo "$TDP < 13" | bc)
+    c=$(echo "$TDP < 15" | bc)
     if [ $c == '1' ]; then
-        $REL_DIR/setTDP.sh 13 --no-config
+        $REL_DIR/setTDP.sh 15 --no-config
     fi
 fi
